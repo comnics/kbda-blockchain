661cc7a9158ce9b8906dbd539fa69314ba69bff5
이민호 <comnics@gmail.com>
2019-04-09 16:27:58 +0900

    Revert "Transaction Basic Class."
    This reverts commit 59249b24e036ca284c6f237f9c66d4dbbcb7e7c5.

--- src/org/kbda/blockchain/KbdaBlockchain.java
+++ src/org/kbda/blockchain/KbdaBlockchain.java
@@ -3,7 +3,6 @@ package org.kbda.blockchain;
 import java.util.ArrayList;
 
 import org.kbda.blockchain.core.Block;
-import org.kbda.blockchain.core.Transaction;
 import org.kbda.blockchain.core.Wallet;
 
 import com.google.gson.GsonBuilder;
@@ -18,12 +17,7 @@ public class KbdaBlockchain {
 	public static void main(String[] arg) {
 		//Wallet 생성
 		Wallet w1 = new Wallet();
-		Wallet w2 = new Wallet();
 		w1.generateKeyPair();
-		w2.generateKeyPair();
-		
-		Transaction t1 = new Transaction(w1.publicKey, w2.publicKey, 100);
-		
 /*
 		//초기 블럭을 만듭니다.
 		blockchain.add(new Block("Genesis block", "0"));
